2601a24440d5b4c60e349fd98b97c68ccdca35fa
yahyavi <yahyavi@gmai.com>
2014-04-15 04:12:45 -0400

    cleanup

--- scripts/startnodes.sh
+++ scripts/startnodes.sh
@@ -1,3 +1,9 @@
+#########################################
+#       GNU GPL License v3              #
+#       Author: Amir Yahyavi            #
+#########################################
+
+#!/bin/bash
 source deploynodes.sh
 
 startnodes
